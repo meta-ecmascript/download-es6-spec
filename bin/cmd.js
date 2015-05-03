f2b3176e12ae5f516bbbffab5dc68d74aaccee46
azu <azuciao@gmail.com>
2015-05-03 23:54:01 +0900

    fix(cmd): correct exit status

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,4 +3,10 @@ var argv = require('minimist')(process.argv.slice(2));
 var downloads = require("../lib/download-es6-spec");
 downloads({
     outDir: argv.D || argv.outDir
+}, function (error, files) {
+    if (error) {
+        console.error(error.stack);
+        process.exit(1);
+    }
+    process.exit(0);
 });
